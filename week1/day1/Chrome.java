aa15bb4a839249bd4eaf47ed8df9b61902bbb505
Siva Ragul <64310579+Sivaragul1@users.noreply.github.com>
2025-08-06 19:27:21 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		Float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("Chrome version:  "+version+ "\n" + "developer: "+developer+ "\n" + "condition: "+isBeta+ "\n" +"release Year:" +releaseYear+ "\n" + "ShortcutKey: " +shortcutKey);
+
+	}
+
+}
