161592690e6d48e0a86cef5af1d4584181f8fbb1
Siva <64310579+Sivaragul1@users.noreply.github.com>
2025-08-19 23:49:26 +0530

    Add files via upload

---
+++ week3/day2/MySqlConnection.java
@@ -0,0 +1,9 @@
+package week3.day2;
+
+public abstract class MySqlConnection implements DatabaseConnection {
+
+    public void executeQuery(){
+        System.out.println("Executed the Query");
+    }
+
+}
